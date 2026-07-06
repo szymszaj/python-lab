2d3701439337a4d53b31af9ef95b74bc633d2122
Szymon Zych <szymonzych868@gmail.com>
2026-07-06 23:20:22 +0200

    Add me.py to define a person object and print details

---
+++ sandbox/1_/me.py
@@ -0,0 +1,11 @@
+person = {
+    "name": "Szymon", 
+    "age": 28,
+    "hobby": "games",
+    "job": "frontend",
+}
+print(person["name"])
+print(person["age"])
+
+print(f"My name is {person['name']}, I work as a {person['job']}, "
+      f"I am {person['age']} years old and I like {person['hobby']}.")
