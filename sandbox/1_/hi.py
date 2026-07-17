2b03a1dc1eca7ff3bdc305e02115fcc5065578c3
Szymon Zych <szymonzych868@gmail.com>
2026-07-17 23:19:30 +0200

    Add hi.py to define greeting and work functions with example usage

---
+++ sandbox/1_/hi.py
@@ -0,0 +1,11 @@
+def hi(name):
+  return f"hello, {name}!"
+
+message = hi("Szymon")
+print(message)
+
+def work(name, job, salary):
+  return f"{name} works as a {job} and earns {salary}."
+
+work_message = work("Szymon", "IT", 1000)
+print(work_message)
