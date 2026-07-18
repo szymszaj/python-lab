8c6c6f2001287803dbc2f7d7cb36477991ac9741
Szymon Zych <szymonzych868@gmail.com>
2026-07-18 22:55:08 +0200

    Add srednia_localne function to calculate average from a local list

--- sandbox/1_/liczba.py
+++ sandbox/1_/liczba.py
@@ -5,3 +5,10 @@ def srednia(liczby):
 
 oceny = [4, 5, 3, 5, 4]
 print(f"Srednia ocen: {srednia(oceny)}")
+
+localne = [1, 2, 3]
+
+def srednia_localne():
+  if len(localne) ==0:
+    return 0
+  return sum(localne) / len(localne)  
